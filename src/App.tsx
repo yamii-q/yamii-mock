76f54d305d9533863ca81d5e1f233ad93f019d00
yamee-dev <ryo.eiyama@icloud.com>
2022-04-05 19:55:15 +0900

    コンポーネント追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,10 @@
-import { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { VFC } from 'react';
+import Footer from './components/Footer';
 
-const App = () => {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-blue-300 bg-red-600">Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  );
-};
+const App: VFC = () => (
+  <footer>
+    <Footer />
+  </footer>
+);
 
 export default App;
