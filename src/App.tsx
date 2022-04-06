86fad195af80554a31227c1aa83765ea128491cd
yamee-dev <ryo.eiyama@icloud.com>
2022-04-07 00:05:19 +0900

    header追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import { VFC } from 'react';
 import Footer from './components/Footer';
+import Header from './components/Header';
 
 const App: VFC = () => (
-  <footer>
-    <Footer />
-  </footer>
+  <>
+    <header>
+      <Header />
+    </header>
+
+    <footer>
+      <Footer />
+    </footer>
+  </>
 );
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,235 @@
+import { VFC } from 'react';
+
+const Header: VFC = () => (
+  <div className="bg-white lg:pb-12">
+    <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+      <header className="flex items-center justify-between py-4 md:py-8">
+        <a
+          href="/"
+          className="text-black-800 inline-flex items-center gap-2.5 text-2xl font-bold md:text-3xl"
+          aria-label="logo"
+        >
+          <svg
+            width="95"
+            height="94"
+            viewBox="0 0 95 94"
+            className="h-auto w-6 text-indigo-500"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M96 0V47L48 94H0V47L48 0H96Z" />
+          </svg>
+          EiRingo
+        </a>
+
+        <nav className="hidden gap-12 lg:flex">
+          <a
+            href="#"
+            className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700"
+          >
+            Home
+          </a>
+          <a
+            href="#"
+            className="inline-flex items-center gap-1 text-lg font-semibold text-indigo-500"
+          >
+            Features
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 text-gray-800"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </a>
+          <a
+            href="#"
+            className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700"
+          >
+            Pricing
+          </a>
+          <a
+            href="#"
+            className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700"
+          >
+            About
+          </a>
+        </nav>
+
+        <div className="-ml-8 hidden flex-col gap-2.5 sm:flex-row sm:justify-center lg:flex lg:justify-start">
+          <a
+            href="#"
+            className="inline-block rounded-lg px-4 py-3 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:text-indigo-500 focus-visible:ring active:text-indigo-600 md:text-base"
+          >
+            Sign in
+          </a>
+
+          <a
+            href="#"
+            className="inline-block rounded-lg bg-indigo-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 md:text-base"
+          >
+            Sign up
+          </a>
+        </div>
+
+        <button
+          type="button"
+          className="inline-flex items-center gap-2 rounded-lg bg-gray-200 px-2.5 py-2 text-sm font-semibold text-gray-500 ring-indigo-300 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base lg:hidden"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+              clipRule="evenodd"
+            />
+          </svg>
+          Menu
+        </button>
+      </header>
+
+      <div className="hidden w-full overflow-hidden rounded-lg border bg-gray-50 shadow-sm lg:block">
+        <div className="mx-auto flex max-w-screen-lg items-center gap-8 p-8">
+          <div className="grid w-2/3 grid-cols-2 gap-8">
+            <a href="#" className="group flex gap-4">
+              <div className="flex h-10 w-10 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg transition duration-100 group-hover:bg-indigo-600 group-active:bg-indigo-700 md:h-12 md:w-12">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"
+                  />
+                </svg>
+              </div>
+
+              <div>
+                <div className="mb-1 font-semibold">Growth</div>
+                <p className="text-sm text-gray-500">
+                  This is a section of some simple filler text, also known as
+                  placeholder text.
+                </p>
+              </div>
+            </a>
+
+            <a href="#" className="group flex gap-4">
+              <div className="flex h-10 w-10 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg transition duration-100 group-hover:bg-indigo-600 group-active:bg-indigo-700 md:h-12 md:w-12">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
+                  />
+                </svg>
+              </div>
+
+              <div>
+                <div className="mb-1 font-semibold">Security</div>
+                <p className="text-sm text-gray-500">
+                  This is a section of some simple filler text, also known as
+                  placeholder text.
+                </p>
+              </div>
+            </a>
+
+            <a href="#" className="group flex gap-4">
+              <div className="flex h-10 w-10 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg transition duration-100 group-hover:bg-indigo-600 group-active:bg-indigo-700 md:h-12 md:w-12">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M3 15a4 4 0 004 4h9a5 5 0 10-.1-9.999 5.002 5.002 0 10-9.78 2.096A4.001 4.001 0 003 15z"
+                  />
+                </svg>
+              </div>
+
+              <div>
+                <div className="mb-1 font-semibold">Cloud</div>
+                <p className="text-sm text-gray-500">
+                  This is a section of some simple filler text, also known as
+                  placeholder text.
+                </p>
+              </div>
+            </a>
+
+            <a href="#" className="group flex gap-4">
+              <div className="flex h-10 w-10 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg transition duration-100 group-hover:bg-indigo-600 group-active:bg-indigo-700 md:h-12 md:w-12">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M2 11a1 1 0 011-1h2a1 1 0 011 1v5a1 1 0 01-1 1H3a1 1 0 01-1-1v-5zM8 7a1 1 0 011-1h2a1 1 0 011 1v9a1 1 0 01-1 1H9a1 1 0 01-1-1V7zM14 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1h-2a1 1 0 01-1-1V4z" />
+                </svg>
+              </div>
+
+              <div>
+                <div className="mb-1 font-semibold">Analytics</div>
+                <p className="text-sm text-gray-500">
+                  This is a section of some simple filler text, also known as
+                  placeholder text.
+                </p>
+              </div>
+            </a>
+          </div>
+
+          <div className="w-1/3 overflow-hidden rounded-lg border">
+            <div className="h-48 bg-gray-100">
+              <img
+                src="https://images.unsplash.com/photo-1619118884592-11b151f1ae11?auto=format&q=75&fit=crop&w=320"
+                loading="lazy"
+                alt="Photo by Fakurian Design"
+                className="h-full w-full object-cover object-center"
+              />
+            </div>
+
+            <div className="flex items-center justify-between gap-2 bg-white p-3">
+              <p className="text-sm text-gray-500">
+                This is some simple filler text.
+              </p>
+
+              <a
+                href="#"
+                className="inline-block shrink-0 rounded-lg border bg-white px-3 py-1 text-sm font-semibold text-indigo-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-50 focus-visible:ring active:bg-gray-100"
+              >
+                More
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Header;
